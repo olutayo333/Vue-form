a33996e13fa4a407720d538fe6fdbb9ff2a4f5de
Ayanrinde Olutayo Stephen <olutayostephen@gmail.com>
2024-07-31 09:01:37 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Form1 from './components/Form1.vue' 
+</script>
+
+<template>
+  <Form1/>
+  
+  <!-- <HelloWorld msg="You did it!" />  
+  <TheWelcome /> -->
+ 
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Form1.vue
@@ -0,0 +1,33 @@
+<template>
+    <h5></h5>
+    <div class="container">
+        <div class="row"> 
+            <div class="shadow rounded">
+                <form action="" @submit.prevent="signup">
+                    <input type="text" class="form-control my-2" placeholder="Name" v-model="name" />
+                    <input type="email" class="form-control my-2" placeholder="Email" v-model="email" />
+                    <input type="number" class="form-control my-2" placeholder="Phone Number" v-model="phonenumber" />
+                    <input type="password" class="form-control my-2" placeholder="Password" v-model="password" />
+                    <p>I agree with <a href="">Terms and Conditions</a><span><input type="checkbox" v-model="terms"></span></p>
+                    <button :disabled="!terms" type="submit" class="btn btn-dark mx-auto my-2"> Submit </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+
+    const name= ref("");
+    const email = ref("");
+    const phonenumber = ref("");
+    const password = ref("");
+    const terms = ref(false);
+
+   const signup = ()=>{
+        let obj = {name, email, phonenumber, password, terms}
+        console.log(obj);
+         localStorage.setItem('form1', JSON.stringify(obj));
+    }
+</script>
